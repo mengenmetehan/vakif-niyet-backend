bd4e22a524fda1d8411e4e63bd09a275effd5158
mengen <metehanmengen@gmail.com>
2026-04-10 01:14:49 +0300

    add sound for notification

--- src/main/kotlin/com/vakitniyet/notification/service/ApnsService.kt
+++ src/main/kotlin/com/vakitniyet/notification/service/ApnsService.kt
@@ -79,7 +79,7 @@ class ApnsService(
         }
 
         val token = deviceToken.replace("[^a-fA-F0-9]".toRegex(), "")
-        val payload = """{"aps":{"alert":{"title":"$title","body":"$body"},"sound":"default"}}"""
+        val payload = """{"aps":{"alert":{"title":"$title","body":"$body"},"sound":"salat_hadra.caf"}}"""
         val jwt = buildJwt()
 
         val request = HttpRequest.newBuilder()
